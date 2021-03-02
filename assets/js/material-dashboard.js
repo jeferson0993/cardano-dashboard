f54009073a0aa6d3246c72cb5f5c45d28ba88069
Jeferson Ferreira <jeferson0993@gmail.com>
2021-03-01 22:29:52 -0300

    Update material-dashboard.js
    corrigido datas com dias menores que 0 ( zero );

--- assets/js/material-dashboard.js
+++ assets/js/material-dashboard.js
@@ -223,7 +223,13 @@ md = {
 
       let past_day, low = 999, high = 0, _low = 0, _high = 0;
       for (let index = 6; index >= 0; index--) {
-        past_day = Number(date.getDate()) - index + '-' + Number(date.getMonth() + 1) + '-' + Number(date.getFullYear());
+        
+        if (Number(date.getDate()) - index <= 0) {
+            past_day = Number(date.getDate()) - index + 30 + '-' + Number(date.getMonth()) + '-' + Number(date.getFullYear());
+        } else {
+            past_day = Number(date.getDate()) - index + '-' + Number(date.getMonth() + 1) + '-' + Number(date.getFullYear());
+        }
+        
         dataPriceChart.labels.push(past_day);
         
         fetch(`https://api.coingecko.com/api/v3/coins/cardano/history?date=${past_day}&localization=false`)
